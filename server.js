41f9ace157d72f5935b09b4a92510783c4b11cc9
Isaías <isaias@artikaweb.com>
2021-06-05 23:25:33 +0200

    texto modificado

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
-app.get('/', (req,res)=> res.send('Hola mundo con express!'));
+app.get('/', (req,res)=> res.send('<h1>Express con HTML</h1>'));
 app.listen(3000);
 console.log('Server on port 3000');
